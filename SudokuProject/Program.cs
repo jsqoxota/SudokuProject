8246c0d44329b77032dc937aaa1ab16f8a6fc1bc
jsq <13960760152@163.com>
2017-09-06 22:08:41 +0800

    Sudoku

---
+++ SudokuProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokuProject.SudokuProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
